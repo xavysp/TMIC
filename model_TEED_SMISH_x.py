bc886877845a67c698d920510e7bed9b99cb745e
xavysp <xsoriap@yahoo.com>
2024-10-08 10:35:23 -0500

    Proposed models updated with augmentation

---
+++ model_TEED_SMISH_x.py
@@ -0,0 +1,63 @@
+"""
+By: xavysp
+This architecture comes from TEED. It is used
+for  image classification
+The official TMIC architecture with 2 TEED's blocks
+"""
+import keras
+import tensorflow as tf
+
+def k_smish2(x):
+    inp = x
+    xDot = keras.ops.tanh(keras.ops.log(1+keras.ops.sigmoid(x)))
+    return inp * xDot
+
+class k_smish(keras.layers.Layer):
+    def call(self, inp):
+        x= inp
+        xDot = tf.math.tanh(tf.math.log(1+tf.sigmoid(x)))
+        return inp * xDot
+
+def data_augmentation(images):
+    data_augmentation_layers = [
+        keras.layers.RandomFlip("horizontal_and_vertical"),
+        keras.layers.RandomContrast(0.2)
+    ]
+    for layer in data_augmentation_layers:
+        images = layer(images)
+    return images
+def model_maker(input_shape, num_classes):
+    input = keras.Input(shape=input_shape)# [None, 28,28,1]
+    x = data_augmentation(input)
+    # x = keras.layers.Rescaling(1. / 255)(x)
+    # TMIC size
+    #tmic_size->  48=small, 128 = medium, 256 = large
+    m_size = 48 # model size from the third block
+    f_size = 128 #if m_size==48 else 128
+    # block 1
+    x = keras.layers.Conv2D(16,3, strides=2, padding="same")(input) # [None, 14,14,16]
+    x = k_smish()(x)
+    x = keras.layers.Conv2D(16,3, strides=1, padding="same")(x)
+    x = k_smish()(x)
+
+    # Block 2
+    px = keras.layers.MaxPooling2D(3,2,"same")(x)
+    px = keras.layers.Conv2D(32, 3, padding="same")(px) # [None, 7,7,32]
+    px = k_smish()(px)
+    px = keras.layers.Conv2D(32, 3, padding="same")(px)
+    px = k_smish()(px)
+
+    px = keras.layers.Dropout(0.25)(px)
+
+
+    # flatten
+    ex = keras.layers.Flatten()(px)
+    # We specify activation=None so it return logits
+    ex = keras.layers.Dense(f_size,activation=k_smish2)(ex)
+    ex= keras.layers.BatchNormalization()(ex)
+    ex = keras.layers.Dropout(0.5)(ex)
+    output = keras.layers.Dense(num_classes,activation="softmax")(ex)
+    # output = keras.layers.Dense(units, activation=None)(x)
+    return keras.Model(input, output)
+
+
